e03062ec90c32eb81cdd79fb9de7f93e5816b0fa
Juan Pablo <avilanjuanpablo@gmail.com>
2022-09-01 15:39:36 -0500

    Laboratorio Constraint Layout

---
+++ app/src/main/java/co/edu/unipiloto/constraintlayoutlab/ExerciseC.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.constraintlayoutlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExerciseC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_exercise);
+    }
+}
---
+++ app/src/main/java/co/edu/unipiloto/constraintlayoutlab/ExerciseD.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.constraintlayoutlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExerciseD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d_exercise);
+    }
+}
